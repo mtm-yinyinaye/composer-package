6768f5c781a14f62a7b02cce3ba59061a6099bf4
yinyinayescm <60867210+mtm-yinyinaye@users.noreply.github.com>
2023-08-11 10:05:01 +0630

    init commit

---
+++ src/DarkMode.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Yinyinaye\DarkModePackage;
+
+class DarkMode
+{
+    public static function changeMode($mode)
+    {
+        echo $mode;
+    }
+}
---
+++ src/index.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+// $class = new \Yinyinaye\DarkModePackage\DarkMode();
+// $testMode = $class->changeMode('dark');
